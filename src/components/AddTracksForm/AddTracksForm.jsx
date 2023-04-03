07bf85cf46586a63854360d020f651a0d8eb54c2
connorbyram <connorabyram@gmail.com>
2023-04-03 00:58:00 -0700

    final push

--- src/components/AddTracksForm/AddTracksForm.jsx
+++ src/components/AddTracksForm/AddTracksForm.jsx
@@ -1,17 +1,48 @@
 import { useState, useRef } from "react";
 
 export default function AddTracksForm() {
+    const [tracks, setTracks] = useState([]);
     const fileInputRef = useRef();
+    const titleInputRef = useRef();
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      const title = titleInputRef.current.value;
+      const track = fileInputRef.current.files[0];
+      const number = tracks.length + 1;
+      
+      setTracks((prevTracks) => [...prevTracks, { title, track, number }]);
+      titleInputRef.current.value = "";
+      fileInputRef.current.value = "";
+    };
+  
+
     return (
         <>
-            <form action="">
+            {tracks.map((track, index) => (
+                <div key={index}>
+                    <span>{track.number} - </span>
+                    <span>{track.title}</span>
+                </div>
+            ))}
+            <form onSubmit={handleSubmit}>
+                <label>Track Title</label>
+                <input 
+                  name="title" 
+                  type="text"
+                  ref={titleInputRef}
+                  placeholder="Title"
+                  autoComplete="off" 
+                  required
+                />
                 <label>Add a Track:</label>
                 <input 
                     type="file"
                     name="track"
                     ref={fileInputRef}
-                    accept=".wav, .mp3"
+                    // accept=".wav, .mp3"
                 />
+                <button type="submit">+</button>
             </form>
         </>
     );
